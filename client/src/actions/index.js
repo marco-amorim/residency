d48de37c55521e6aba8189d0aa6ea914d2d26205
marco-amorim <marcoamorim95@hotmail.com>
2020-09-17 00:02:41 -0300

    created actions based on rest conventions and clear db

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,20 +1,38 @@
 import residences from '../apis/residences';
-import { CREATE_RESIDENCE } from '../actions/types';
-
-// export const createResidence = (formValues) => async (dispatch) => {
-// 	residences
-// 		.post('/residences', formValues)
-// 		.then((response) => {
-// 			window.alert('Form sucessfully submitted!');
-// 			console.log(response.status);
-// 		})
-// 		.catch((error) => {
-// 			window.alert('Something went wrong. ' + error);
-// 		});
-// };
+import {
+	CREATE_RESIDENCE,
+	DELETE_RESIDENCE,
+	EDIT_RESIDENCE,
+	FETCH_RESIDENCE,
+	FETCH_RESIDENCES,
+} from '../actions/types';
 
 export const createResidence = (formValues) => async (dispatch) => {
 	const response = await residences.post('/residences', { ...formValues });
 
 	dispatch({ type: CREATE_RESIDENCE, payload: response.data });
+};
+
+export const fetchResidences = () => async (dispatch) => {
+	const response = await residences.get('/residences');
+
+	dispatch({ type: FETCH_RESIDENCES, payload: response.data });
+};
+
+export const fetchResidence = (id) => async (dispatch) => {
+	const response = await residences.get(`/residences/${id}`);
+
+	dispatch({ type: FETCH_RESIDENCE, payload: response.data });
+};
+
+export const editResidence = (id, formValues) => async (dispatch) => {
+	const response = await residences.put(`/residences/${id}`, formValues);
+
+	dispatch({ type: EDIT_RESIDENCE, payload: response.data });
+};
+
+export const deleteResidence = (id) => async (dispatch) => {
+	await residences.delete(`/residences/${id}`);
+
+	dispatch({ type: DELETE_RESIDENCE, payload: id });
 };
