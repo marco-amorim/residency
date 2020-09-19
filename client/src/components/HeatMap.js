96c6322bd8cb7e0310d10e6a90228a098505f1a6
marco-amorim <marcoamorim95@hotmail.com>
2020-09-18 21:47:36 -0300

    trying out heat map with leaflet

--- client/src/components/HeatMap.js
+++ client/src/components/HeatMap.js
@@ -1,8 +1,34 @@
 import React, { Component } from 'react';
+import { Map, TileLayer } from 'react-leaflet';
+import HeatmapLayer from 'react-leaflet-heatmap-layer';
 
 class HeatMap extends Component {
+	componentDidMount() {}
+
+	addressPoints = () => {
+		return [[-27.592464, -48.586949, '571']];
+	};
+
 	render() {
-		return <h2>Heat Map</h2>;
+		return (
+			<React.Fragment>
+				<h2>Heat Map</h2>
+				<Map center={[-27.592464, -48.586949]} zoom={13}>
+					<HeatmapLayer
+						points={this.addressPoints()}
+						longitudeExtractor={(m) => m[1]}
+						latitudeExtractor={(m) => m[0]}
+						intensityExtractor={(m) => parseFloat(m[2])}
+						fitBoundsOnLoad
+						fitBoundsOnUpdate
+					/>
+					<TileLayer
+						url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+						attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+					/>
+				</Map>
+			</React.Fragment>
+		);
 	}
 }
 
